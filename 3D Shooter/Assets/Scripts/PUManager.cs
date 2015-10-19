3533a9a8d6efeed1253b82a02a4e74bf453bf4ea
MarcosIPCA <marcos.mgbr@gmail.com>
2015-10-19 10:53:16 +0100

    Power Up Random Spawner

--- 3D Shooter/Assets/Scripts/PUManager.cs
+++ 3D Shooter/Assets/Scripts/PUManager.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PUManager : MonoBehaviour {
 
-    public GameObject spreadPU;
+    public GameObject[] pickUpList;
     float nextSpawn;
     public float maxSpawnDelay, minSpawnDelay;
+    public GameObject room;
 
 	// Use this for initialization
 	void Start ()
@@ -19,7 +21,11 @@ public class PUManager : MonoBehaviour {
 	    if(Time.time > nextSpawn)
         {
             nextSpawn = Time.time + Random.Range(minSpawnDelay, maxSpawnDelay + 1);
-            PickUp newPU = Instantiate(spreadPU, Vector3.zero, Quaternion.identity) as PickUp;
+
+            float randomX = Random.Range(room.transform.position.x - room.GetComponent<Renderer>().bounds.size.x / 2.5f, room.transform.position.x + room.GetComponent<Renderer>().bounds.size.x / 2.5f);
+            float randomZ = Random.Range(room.transform.position.z - room.GetComponent<Renderer>().bounds.size.x / 2.5f, room.transform.position.z + room.GetComponent<Renderer>().bounds.size.x / 2.5f);
+
+            PickUp newPU = Instantiate(pickUpList[Random.Range(0, pickUpList.Length)], new Vector3(randomX, 0, randomZ), Quaternion.identity) as PickUp;
         }
 	}
 }
