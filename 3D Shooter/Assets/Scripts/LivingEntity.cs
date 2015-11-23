7c58178763321d68f56a690302e20b3d26950ab8
MarcosIPCA <marcos.mgbr@gmail.com>
2015-11-23 10:29:28 +0000

    Health GUI

--- 3D Shooter/Assets/Scripts/LivingEntity.cs
+++ 3D Shooter/Assets/Scripts/LivingEntity.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class LivingEntity : MonoBehaviour, IDamageable {
 
     public float startingHealth;
-    protected float health;
+    public float health;
     protected bool dead;
 
     public Material skinMaterial;
--- 3D Shooter/Assets/Scripts/UIManager.cs
+++ 3D Shooter/Assets/Scripts/UIManager.cs
@@ -4,7 +4,7 @@ using System.Collections;
 
 public class UIManager : MonoBehaviour {
 
-    public GameObject combo, score, ammo, player;
+    public GameObject health, combo, score, ammo, player;
 
 	// Use this for initialization
 	void Start () 
@@ -19,6 +19,8 @@ public class UIManager : MonoBehaviour {
         else combo.GetComponent<Text>().text = "";
 
         score.GetComponent<Text>().text = player.GetComponent<Player>().score.ToString() + " POINTS";
+
+        health.GetComponent<Text>().text = player.GetComponent<Player>().health.ToString();
 
         if(player.GetComponent<Player>().gunController.equippedGun.infinite) ammo.GetComponent<Text>().text = "";
         else ammo.GetComponent<Text>().text = player.GetComponent<Player>().gunController.equippedGun.ammo.ToString() + " BULLETS";
